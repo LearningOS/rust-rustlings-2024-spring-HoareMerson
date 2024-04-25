a9a9df0c6afc6c9f90006deaa7fd7c8352806f49
HoareMerson <1394680594@qq.com>
2024-04-25 15:08:16 +0800

    move_semantics finished

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -10,9 +10,11 @@
 // I AM NOT DONE
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
 
     let mut vec1 = fill_vec(vec0);
+
+    vec0 = vec1.clone();
 
     println!("{} has length {}, with contents: `{:?}`", "vec0", vec0.len(), vec0);
 
